ee7a86a8f4e6631590dfc7e16cdeeaa78e26cb66
Kliment <kliment1986@inbox.ru>
2021-09-12 19:05:44 +0300

    api k8s configs

--- deploy/run-local.sh
+++ deploy/run-local.sh
@@ -1,13 +1,18 @@
-# Применение файлов конфигурации
+# Конфигурирование клиентского приложения
+kubectl apply -f client-cluster-ip-service.yaml
 kubectl apply -f client-deployment.yaml
-kubectl apply -f client-node-port.yaml
+
+# Конфигурирование приложения АПИ
+kubectl apply -f api-cluster-ip-service.yaml
+kubectl apply -f api-deployment.yaml
 
 # Получение списка подов и сервисов
 kubectl get pods
 kubectl get services
 
 # Детальная инфонмация о текущем поде
-kubectl describe pod client-pod
+kubectl describe deployment client-deployment
+kubectl describe deployment api-deployment
 
 
-minikube service client-node-port
+
