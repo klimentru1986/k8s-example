a8f3ac43aaee2c6b67dc3de0707b7f52dc757f8e
Kliment <kliment1986@inbox.ru>
2021-09-25 09:09:17 +0300

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ docker build -t klimentru1986/kubernatestodo-api:latest -t klimentru1986/kuberna
 docker push klimentru1986/kubernatestodo-client
 docker push klimentru1986/kubernatestodo-api
 
-kubectl apply ./k8s/.
+kubectl apply ./deploy/k8s/.
 
 kubectl set image deployments/api-deployment api=klimentru1986/kubernatestodo-api:$GIT_SHA
 kubectl set image deployments/client-deployment client=klimentru1986/kubernatestodo-client:$GIT_SHA
